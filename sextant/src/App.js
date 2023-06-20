47e121026ebbc9d5ec2fdfd470dbdc5704d6c6a7
Nakul Prasad <nakulprasad10@outlook.com>
2023-06-21 01:47:58 +0530

    Task 3

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,9 +1,7 @@
-
-import './App.css';
-
 import React from 'react'
 import Banner from './components/Banner';
 import Exhibit from './components/Exhibit';
+import Address from './components/Address';
 
 function App() {
   return (
@@ -12,9 +10,16 @@ function App() {
       <div className='container'>
 
         <div className="row ">
-          <div className="col"><Exhibit /></div>
-          <div className="col"><Exhibit /></div>
-          <div className="col"><Exhibit /></div>
+          <div className="col mb-5"><Exhibit name="Public IPv4 Address">
+            <Address url='https://api.ipify.org?format=json' />
+          </Exhibit>
+          </div>
+          <div className="col mb-5"><Exhibit name="Public IPv4(v6) Address">
+            <Address url='https://api64.ipify.org?format=json' />
+          </Exhibit>
+          </div>
+
+          <div className="col mb-5"><Exhibit /></div>
 
         </div>
       </div>
--- sextant/src/components/Exhibit.jsx
+++ sextant/src/components/Exhibit.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-function Exhibit() {
+function Exhibit(props) {
   return (
     <div className="container">
       <div className="card" style={{ width: "18rem" }}>
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+          src="https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80"
           className="card-img-top"
           alt="..."
         />
         <div className="card-body">
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          <h2 className="card__title">{props.name}</h2>
+          <p className="card__apply" style={{ color: "blue" }}>
+            {props.children}
           </p>
         </div>
       </div>
